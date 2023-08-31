059d81890f289fe8692bcbcdb192cb0b312de748
Pantoja14022020 <tl419411@uaeh.edu.mx>
2023-08-31 15:31:51 -0600

    mejoras en el home de la app

--- src/public/scripts/armario.js
+++ src/public/scripts/armario.js
@@ -5,3 +5,82 @@ if(armario){
         armario.classList.add('show-interfaz-armario');
     },500);
 }
+
+
+const marcaSeccion = document.getElementById('marca');
+const tallasSeccion = document.getElementById('tallas');
+const categoriaSeccion = document.getElementById('categoria');
+const subcategoriaSeccion = document.getElementById('subcategoria');
+
+
+if(marcaSeccion){
+    marcaSeccion.addEventListener('click',e=>{
+        console.log("click marca")
+    });
+}
+
+if(tallasSeccion){
+    tallasSeccion.addEventListener('click',e=>{
+        console.log("click talla")
+    });
+}
+
+if(categoriaSeccion){
+    categoriaSeccion.addEventListener('click',e=>{
+        console.log("click categoria")
+    });
+}
+
+if(subcategoriaSeccion){
+    subcategoriaSeccion.addEventListener('click',e=>{
+        console.log("click subcategoria")
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const disponiblesSeccion = document.getElementById('disponibles');
+const reparacionSeccion = document.getElementById('reparacion');
+const suciosSeccion = document.getElementById('sucios');
+const lavanderiaSeccion = document.getElementById('lavanderia');
+
+
+if(disponiblesSeccion){
+    disponiblesSeccion.addEventListener('click',e => {
+        console.log("click en disponibles")       
+    });
+}
+
+if(reparacionSeccion){
+    reparacionSeccion.addEventListener('click', e => {
+        console.log("click en reparacion");
+    });
+}
+
+if(suciosSeccion){
+    suciosSeccion.addEventListener('click', e => {
+        console.log("click en sucios")
+    });
+}
+
+if(lavanderiaSeccion){
+    lavanderiaSeccion.addEventListener('click', e => {
+        console.log("click en lavanderia")
+    });
+}
